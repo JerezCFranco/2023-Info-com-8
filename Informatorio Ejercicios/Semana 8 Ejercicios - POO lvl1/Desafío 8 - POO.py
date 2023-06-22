7d0281417c0305cde668248438bde1ebf5806698
JerezCFranco <ermac715@gmail.com>
2023-06-22 15:40:41 -0300

    Todos hasta la semana 9

--- Informatorio Ejercicios/Semana 8 Ejercicios - POO lvl1/Desafío 8 - POO.py
+++ Informatorio Ejercicios/Semana 8 Ejercicios - POO lvl1/Desafío 8 - POO.py
@@ -1,10 +1,3 @@
-# Ejercicio 4: Tiempo
-# Crear una clase Tiempo, con atributos hora, minuto y segundo, que pueda ser
-# instanciada indicando: los tres atributos, sólo la hora y minuto, o sólo la hora.
-# Crear además los métodos necesarios para modificar la hora en cualquier
-# momento de forma manual. Mantenga la integridad de los datos en todo
-# momento definiendo de tipo “private”. Crear una clase prueba_tiempo que
-# prueba una hora concreta y la modifique a su gusto mostrándola por pantalla.
 
 class Usuario:
     def __init__(self, id_usuario, nombre, apellido, teléfono, username, email, contraseña, fecha_de_registro, avatar, estado, online):
